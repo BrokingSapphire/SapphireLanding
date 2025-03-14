7dc052f8e468b590d9a12405966c637b9589e9b2
deevee47 <divyanshvishwakarma4@gmail.com>
2025-03-15 01:31:40 +0530

    feat: sidebar

--- src/components/general-components/SubHero.tsx
+++ src/components/general-components/SubHero.tsx
@@ -12,18 +12,19 @@ const SubHero = ({ chipText, titleText, gradientText, description }: SubHeroProp
   return (
     <div className="relative h-[400px] mt-10 w-full flex flex-col items-center justify-center mx-auto px-8">
       <Image
-        src="/bg-image.png"
+        src="/hero-element.svg"
         alt="Decorative Image"
-        width={250}
+        width={1000}
         height={250}
-        className="absolute top-8 w-24 sm:w-full left-0 "
+        draggable={false} 
+        className="sm:absolute sm:block top-8 hidden sm:w-64 left-0 "
       />
 
       <div className="w-full space-y-5 text-center">
         <span className="bg-[#E4FFEE] text-green-heading px-4 py-2 rounded-3xl">
           {chipText}
         </span>
-        <div className="text-center font-semibold leading-tight text-6xl w-full">
+        <div className="text-center font-semibold leading-tight text-4xl sm:text-6xl w-full">
           {titleText[0]} <br />{" "}
           <span className="font-lobster-two pr-2 italic text-transparent tracking-wider bg-clip-text font-[400] bg-gradient-to-r from-[#14A5AD] to-[#21B906]">
             {gradientText}
