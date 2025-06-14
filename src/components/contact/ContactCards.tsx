497e89af46218a50138f38b9a7d35525e8a11e14
sapphirebroking <nakul.thakur@sapphirebroking.com>
2025-06-14 13:38:41 +0530

    fix: address

--- src/components/contact/ContactCards.tsx
+++ src/components/contact/ContactCards.tsx
@@ -56,9 +56,7 @@ const HQCard: React.FC = () => (
         </h3>
       </div>
       <p className="text-sm sm:text-base text-gray-600">
-      📍 Plot No. 84-A, First Floor, East Side (Front), Pande Layout, New Sneh Nagar, Khamla, Nagpur, Maharashtra
-        <br />
-        Pincode : 440025
+      📍 Plot No. 84-A, First Floor, East Side (Front), Pande Layout, New Sneh Nagar, Khamla, Nagpur, Maharashtra - 440025
       </p>
     </div>
   </div>
