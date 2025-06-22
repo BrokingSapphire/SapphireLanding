6342c60213baa553deb17c4a17d1e1f503edf097
sapphirebroking <nakul.thakur@sapphirebroking.com>
2025-06-22 17:36:48 +0530

    fix: remove sapphire journey image

--- src/components/about/SapphireJourney.tsx
+++ src/components/about/SapphireJourney.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import Image from 'next/image';
+// import React from 'react';
+// import Image from 'next/image';
 
-const SapphireJourney = () => {
-  return (
-    <>
-      <Image
-        src="/about/journey.svg"
-        width={1500}
-        height={1500}
-        alt="journey"
-        draggable={false} // ✅ disables drag
-        className="select-none mx-auto" // optional: prevents image from being selected
-      />
-    </>
-  );
-};
+// const SapphireJourney = () => {
+//   return (
+//     <>
+//       <Image
+//         src="/about/journey.svg"
+//         width={1500}
+//         height={1500}
+//         alt="journey"
+//         draggable={false} // ✅ disables drag
+//         className="select-none mx-auto" // optional: prevents image from being selected
+//       />
+//     </>
+//   );
+// };
 
-export default SapphireJourney;
+// export default SapphireJourney;
