9e27093cd1466fde0a8aa4a3a8e90aa9adb65651
sapphirebroking <nakul.thakur@sapphirebroking.com>
2025-07-07 12:43:57 +0530

    feat: add matadata to account/fund-transfer

--- src/app/(account)/fund-transfer/page.tsx
+++ src/app/(account)/fund-transfer/page.tsx
@@ -1,6 +1,22 @@
+import { Metadata } from "next";
 import React from "react";
 
-// Bank Account data structure
+export const metadata: Metadata = {
+  title: "Fund Transfer | Sapphire Broking: Smarter Trading, Expert Insights",
+  description:
+    "Sapphire has a next-generation trading platform designed for investors seeking expert insights and advanced trading tools. Get professional trade recommendations and stay updated with real-time corporate announcements from all listed entities. Our high-tech features ensure seamless execution, empowering both beginners and experienced traders to make well-informed market decisions.",
+  keywords:
+    "about sapphire broking, online trading platform India, stock market analysis tools, real-time market insights, professional trading recommendations, BSE NSE live updates, smart trading solutions, expert trading insights, stock market for beginners, advanced trading platform, corporate announcement tracker, investment decision tools, market intelligence platform, stock trading alerts India, seamless trade execution, next-gen trading platform, stock market education, technical analysis tools India, fundamental analysis platform, market trend analyzer, financial market insights, algorithmic trading India, trading platform comparison, best online broker India, intraday trading platform, portfolio management tools, stock screening tools India, equity research platform, investment analytics India, market data analysis, trading charts and indicators, mobile trading app India, derivatives trading platform, commodity trading solutions, forex trading tools India",
+  openGraph: {
+    title: "Fund Transfer | Sapphire Broking: Smarter Trading, Expert Insights",
+    description:
+      "Sapphire has a next-generation trading platform designed for investors seeking expert insights and advanced trading tools. Get professional trade recommendations and stay updated with real-time corporate announcements from all listed entities. Our high-tech features ensure seamless execution, empowering both beginners and experienced traders to make well-informed market decisions.",
+    url: "https://sapphirebroking.com/fund-transfer",
+    images: [{ url: "https://www.sapphirebroking.com/logo-white.svg" }],
+    type: "website",
+  },
+};
+
 const bankAccountData = [
   {
     id: "fund-deposit",
